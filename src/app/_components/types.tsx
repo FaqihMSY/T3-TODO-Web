ae17a1298e90bd8e6461745cb186b3986715c719
FAQIH MUHAMMAD SYUHADA <19623083@std.stei.itb.ac.id>
2024-07-23 18:14:42 +0700

    feat : add new feature priority to Task type

--- src/app/_components/types.tsx
+++ src/app/_components/types.tsx
@@ -1,5 +1,6 @@
 export interface Task {
-    id: number;
-    name: string;
-    completed: boolean;
-  }
+  id: number;
+  name: string;
+  completed: boolean;
+  priority: "high" | "medium" | "low";
+}
